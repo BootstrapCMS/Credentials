87c428137dcc94f0e6e8a6b7c806d95de865f44f
Graham Campbell <graham@mineuk.com>
2013-12-26 20:15:50 +0000

    Added facade tests

---
+++ tests/GrahamCampbell/Tests/Credentials/Facades/UserProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of Laravel Credentials by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace GrahamCampbell\Tests\Credentials\Facades;
+
+/**
+ * This is the user provider facade test case class.
+ *
+ * @package    Laravel-Credentials
+ * @author     Graham Campbell
+ * @copyright  Copyright 2013 Graham Campbell
+ * @license    https://github.com/GrahamCampbell/Laravel-Credentials/blob/develop/LICENSE.md
+ * @link       https://github.com/GrahamCampbell/Laravel-Credentials
+ */
+class UserProviderTest extends AbstractFacadeTestCase
+{
+    /**
+     * Get the facade accessor.
+     *
+     * @return string
+     */
+    protected function getFacadeAccessor()
+    {
+        return 'userprovider';
+    }
+
+    /**
+     * Get the facade class.
+     *
+     * @return string
+     */
+    protected function getFacadeClass()
+    {
+        return 'GrahamCampbell\Credentials\Facades\UserProvider';
+    }
+
+    /**
+     * Get the facade route.
+     *
+     * @return string
+     */
+    protected function getFacadeRoot()
+    {
+        return 'GrahamCampbell\Credentials\Providers\UserProvider';
+    }
+}
