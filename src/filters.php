84784008d69c37193d9079c754a26bb0e952d20e
Graham Campbell <graham@mineuk.com>
2014-01-22 22:56:05 +0000

    More filter fixes

--- src/filters.php
+++ src/filters.php
@@ -34,21 +34,21 @@ Route::filter('credentials', function ($route, $request, $value) {
 
 Route::filter('throttle.login', function ($route, $request) {
     if (!Throttle::hit($request, 10, 10)->check()) {
-        Session::flash('error', 'You have made too many login request. Please try again in 10 minutes.');
-        return Redirect::route('account.login')->withErrors($val)->withInput();
+        Session::flash('error', 'You have made too many login requests. Please try again in 10 minutes.');
+        return Redirect::route('account.login')->withInput();
     }
 });
 
 Route::filter('throttle.reset', function ($route, $request) {
     if (!Throttle::hit($request, 5, 30)->check()) {
         Session::flash('error', 'Your computer has been suspended from resetting passwords. Please contact support.');
-        return Redirect::route('account.login')->withErrors($val)->withInput();
+        return Redirect::route('account.login')->withInput();
     }
 });
 
 Route::filter('throttle.register', function ($route, $request) {
     if (!Throttle::hit($request, 10, 30)->check()) {
         Session::flash('error', 'Your computer has been suspended from registration. Please contact support.');
-        return Redirect::route('account.login')->withErrors($val)->withInput();
+        return Redirect::route('account.login')->withInput();
     }
 });
