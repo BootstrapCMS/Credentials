b3cda276ebd70db5f03c4c8fadd6920b8fe24c36
Graham Campbell <graham@mineuk.com>
2014-07-19 11:04:46 +0100

    Added missing displayers

---
+++ src/Presenters/RevisionDisplayers/User/DeletedAtDisplayer.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * This file is part of Laravel Credentials by Graham Campbell.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace GrahamCampbell\Credentials\Presenters\RevisionDisplayers\User;
+
+/**
+ * This is the deleted at displayer class.
+ *
+ * @package    Laravel-Credentials
+ * @author     Graham Campbell
+ * @copyright  Copyright 2013-2014 Graham Campbell
+ * @license    https://github.com/GrahamCampbell/Laravel-Credentials/blob/master/LICENSE.md
+ * @link       https://github.com/GrahamCampbell/Laravel-Credentials
+ */
+class DeletedAtDisplayer extends AbstractDisplayer
+{
+    /**
+     * Get the change title.
+     *
+     * @return string
+     */
+    public function title()
+    {
+        return 'Account Deleted';
+    }
+
+    /**
+     * Get the change description from the context of
+     * the change being made to the current user.
+     *
+     * @return string
+     */
+    protected function current()
+    {
+        if ($this->author() === ' ') {
+            'You deleted your account.';
+        }
+
+        return $this->author() . 'deleted your account.';
+    }
+
+    /**
+     * Get the change description from the context of
+     * the change not being made to the current user.
+     *
+     * @return string
+     */
+    protected function external()
+    {
+        if ($this->wasActualUser()) {
+            return 'The user deleted their account.';
+        }
+
+        if ($this->author() === ' ') {
+            return 'The user deleted their account.';
+        }
+
+        return $this->author() . 'deleted the user\'s account.';
+    }
+}
